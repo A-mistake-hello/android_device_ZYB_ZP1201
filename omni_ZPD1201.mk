96b8fc6b4de4b3c17cae13b2f34257f3d35dd56a
A-mistake-hello <3329989998@qq.com>
2025-04-19 05:07:02 +0800

    Update omni_ZPD1201.mk

--- omni_ZPD1201.mk
+++ omni_ZPD1201.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common twrp stuff.
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from ZPD1201 device
 $(call inherit-product, device/zyb/ZPD1201/device.mk)
